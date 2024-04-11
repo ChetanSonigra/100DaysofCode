d66ec4122f62eca55dceba8d6525cdb4c83c0cae
ChetanSonigra <sonigrachetan44@gmail.com>
2024-04-11 18:51:27 +0530

    Adds Day1 - BandNameGenerator

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,9 @@
+# print, input, string,variables, comment,len()
+# print: sep=' ', end='\n', escaping ' or "  with \ 
+# variables can start with _ or letters and contains only _, letters and digits. 
+# variables are case sensitive and shouldn't be python keywords 
+
+print('Welcome to the band name generator.')
+city = input('Which city did you grow up in?')
+pet = input('What is your pet name?')
+print('Your band name could be: ',city,pet)
