106e23cbede5e99959f20d4e4d61087c82d686f5
ChetanSonigra <sonigrachetan44@gmail.com>
2024-04-12 13:37:07 +0530

    Adds raw string, triple quotes and sys.argv

--- Day1/BandNameGenerator.py
+++ Day1/BandNameGenerator.py
@@ -7,3 +7,23 @@ print('Welcome to the band name generator.')
 city = input('Which city did you grow up in?')
 pet = input('What is your pet name?')
 print('Your band name could be: ',city,pet)
+
+print("""\
+Usage: thingy [OPTIONS]
+-h Display this usage message
+-H hostname Hostname to connect to
+""")
+
+print("""
+Usage: thingy [OPTIONS]
+-h Display this usage message
+-H hostname Hostname to connect to
+""")
+
+print(r'C\SF\SFDS\SDF') # print('r'C\SF\SFDS\SDF\') - odd number of \ at end of raw string is not valid.
+
+# invoking python iterpreter:
+# 1. when invoked with "python" without filename or arguments. sys.argv will be ['']
+# 2. when invoked with "python -" sys.argv will be ['-']
+# 3. when invoked with "python -c 'print(43)'" sys.argv will be ['-c']
+# 4. when invoked with "python -m Day3.control_flow" sys.argv will be ['full path of module']
