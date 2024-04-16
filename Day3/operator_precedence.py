7d34f4aadf77c8ae4bf0908efa84d2757ae307e4
ChetanSonigra <sonigrachetan44@gmail.com>
2024-04-17 03:05:15 +0530

    Adds Operator precedence

---
+++ Day3/operator_precedence.py
@@ -0,0 +1,22 @@
+'''
+Operators in Python have different precedence levels, determining the order of operations in expressions.
+From highest to lowest precedence:
+Parentheses (expressions...), [expressions...], {key: value...}, {expressions...}: Binding or parenthesized expression
+Subscription x[index], Slicing x[index:index]
+await x: Await expression.
+**: Exponentiation.
++x, -x, ~x: Positive, negative, bitwise NOT.
+*, /, //, %: Multiplication, division, floor division, remainder.
++, -: Addition and subtraction.
+<<, >>: Shifts.
+&: Bitwise AND.
+^: Bitwise XOR.
+|: Bitwise OR.
+in, not in, is, is not, <, <=, >, >=, !=, ==: Comparisons, including membership tests and identity tests.
+not x: Boolean NOT.
+and: Boolean AND.
+or: Boolean OR.
+if – else: Conditional expression.
+lambda: Lambda expression.
+:=: Assignment expression.
+'''
