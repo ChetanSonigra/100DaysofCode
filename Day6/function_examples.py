4fbe910fe81c7fd88ded6de1899765f110bfd116
ChetanSonigra <sonigrachetan44@gmail.com>
2024-04-19 02:30:37 +0530

    Adds fucntion basics and practice challanges

---
+++ Day6/function_examples.py
@@ -0,0 +1,12 @@
+def greetings():
+    print('Hello World')
+    return 'Greetings!'
+    
+
+greetings()
+# See below link for styling in python. It explains use of spaces/indentantions in various scenarios.
+# https://peps.python.org/pep-0008/
+
+# Use below link to practice functions, loops, if else and logic building with fun.
+# https://reeborg.ca/reeborg.html
+
