34aeb59bb19deacf7602f232b9e9aed0d25a7894
ChetanSonigra <sonigrachetan44@gmail.com>
2024-05-05 11:55:08 +0530

    Improved password manager to use json file and exception handling.

--- Day29/main.py
+++ Day29/main.py
@@ -3,7 +3,7 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-import pyperclip
+import pyperclip,json
 from tkinter import *
 import tkinter.messagebox as tkm
 from password_generator import generate_password
@@ -12,6 +12,16 @@ window = Tk()
 window.title('Password Manager')
 # window.minsize(240,240)
 window.config(padx=20,pady=20)
+
+
+def search_website():
+    website = application_input.get()
+    try:
+        with open('Day29/data.json','r') as f:
+            data_dict = json.load(f)
+            tkm.showinfo(title=website,message=f"Email: {data_dict[website]['email']}\nPassword: {data_dict[website]['password']}")
+    except:
+        tkm.showinfo(title='Error', message=f"Data for {website} doesn't exist")   
 
 def show_password():
     password= generate_password()
@@ -23,6 +33,7 @@ def add_to_file():
     website = application_input.get()
     email = username_input.get()
     passwd = password_input.get()
+    new_data = {website:{'email': email,'password': passwd}}
     if len(passwd)==0 or len(website)==0:
         tkm.showinfo(title='Oops',message=f"Make sure you haven't left any field empty.")
     else:
@@ -30,9 +41,15 @@ def add_to_file():
                                                     f"\nPassword: {passwd}\nIs it okay to save?"
                                                     )
         if is_ok:
-            with open('Day29/password_manager.txt','+a') as f:
-                f.write(f'{website}    |    {email}    |    {passwd}\n')
-
+            try: 
+                with open('Day29/data.json','r') as f:
+                    data = json.load(f)
+                    data.update(new_data)
+                    # print(data)
+            except:
+                data = new_data
+            with open('Day29/data.json','w') as f:
+                json.dump(data,f,indent=4)
             application_input.delete(0,END)
             password_input.delete(0,END)
 
@@ -45,9 +62,12 @@ canvas.grid(column=1,row=0)
 application= Label(text='Website: ',font=('Arial',12,'normal'))
 application.grid(column=0,row=1)
 
-application_input= Entry(width=53)
-application_input.grid(column=1,row=1,columnspan=2)
+application_input= Entry(width=34)
+application_input.grid(column=1,row=1)
 application_input.focus()
+
+search_button = Button(text='Search',command=search_website,width=15)
+search_button.grid(column=2,row=1)
 
 username= Label(text='Email/Username: ',font=('Arial',12,'normal'))
 username.grid(column=0,row=2)
