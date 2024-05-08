f018dbf111ed6b7c75483c70a185d4c831136135
ChetanSonigra <sonigrachetan44@gmail.com>
2024-05-08 18:58:43 +0530

    Adds advance function topics - Iterator,Generator,Decorator,Lambda

--- Day10/functions_with_return.py
+++ Day10/functions_with_return.py
@@ -31,3 +31,84 @@ multiline comment
 # to access global variables, use 'global'. 
 # To access variables of enclosing function, use 'nonlocal'.
 
+# function as object: f = display; f()
+# function as parameter: f(add,3,5)
+# nested function: outer,inner
+# return function: s = outer(); s()
+
+# def function(a,b,*args,c,**kwargs) 
+# arguments before args must be positional and after args must be keyword
+# def function(a,b,/,c,d,*,e,f) positional, any, keyword
+
+# globals() - gives global variables
+# locals() - gives local variables
+
+# Iterators: i = iter(sequence); next(i);next(i)
+# Generators: 
+def gen():
+    l = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+    i= 0
+    while True:
+        i = i%7
+        x = l[i]
+        i +=1
+        yield x
+
+g = gen() 
+next(g)
+next(g)
+
+# Closure function
+def Closure(msg):
+    greeting = 'Good day!'
+    def display():
+        print('*'*10)
+        print(greeting,msg)   # can access nonlocal variable 
+        print('*'*10)
+    return display
+
+d = Closure('Hellow')
+d()
+
+# Caller Class: 
+# to make class/object callable with __call__ method
+# can be converted into closure function.
+
+# Decorator: 
+def decorate(func):
+    def wrapper(msg):
+        print('*'*10)
+        func(msg)
+        print('*'*10)
+    return wrapper
+
+def display(name):
+    print('Hello',name)
+    
+d = decorate(display)
+d('Chetan')
+
+@decorate
+def display(name):
+    print('Hello',name)
+
+display('Chetan')
+
+
+# Lambda: 
+k = lambda miles: miles*1.6
+
+print(k(10))
+
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+
+final_list = list(filter(lambda x: (x % 2 != 0), li))
+print(final_list)
+
+final_list = list(map(lambda x: x*2, li))
+print(final_list)
+
+import functools
+
+sum = functools.reduce((lambda x, y: x + y), li)
+print(sum)
