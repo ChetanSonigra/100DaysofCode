f043407b86c00fbf822a795b7d7abf705c2de4ef
Chetan <sonigrachetan44@gmail.com>
2024-06-10 23:41:57 +0530

    Adds setup of selenium and driver

---
+++ Day48/main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+# Keep the browser open after program finishes.
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.amazon.com")
+
+# driver.close() # closes single tab
+# driver.quit()  # quit entire browser
+
